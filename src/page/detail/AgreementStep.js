8956865447a280e125229cdc660289458c0f8f00
yoohwajoo <hwahjoo@gmail.com>
2024-03-12 17:16:00 +0900

    방문신청 동의/완료 페이지 코드, css 수정

--- src/page/detail/AgreementStep.js
+++ src/page/detail/AgreementStep.js
@@ -1,14 +1,15 @@
 import { useState } from "react";
+import './AgreementStep.css';
 
 function AgreementStep({onNext,setStep}){
     const [allChk, setAllchk] = useState(false);
-    const [chk1, setChk1] = useState(true);
+    const [chk1, setChk1] = useState(false);
     const [chk2, setChk2] = useState(false);
-    
+
     function handleAllChkChange(){
         setAllchk(prev => !prev)
         setChk1(true)
-        setChk2(prev => !prev)
+        setChk2(true)
     }
     function handleNextBtnChange(){
         if(allChk===true || (chk1===true && chk2===true)){
@@ -16,23 +17,41 @@ function AgreementStep({onNext,setStep}){
             
         }else{
             setStep(0);
-            alert('약관 동의를 확인해주세요');
+            alert('약관 동의를 확인해주세요. \n서비스를 이용하기 위해서는 동의가 필요합니다.');
         }
     }
     return(
         <div>
-            <section>
-                <h3>약관 동의</h3>
-                <p>약관 및 개인정보 수집 이용에 동의 해주세요</p>
-            
-                <div className="flex">
-                    <input type="checkbox" checked={allChk} onChange={handleAllChkChange}/>
-                    <p>이용약관 동의(필수) 및 개인정보 수집 이용 동의(필수)에 모두 동의합니다.</p>
+            <section className="agree_progress">
+                <div className="progress_wrap">
+                    <div>
+                        <p className="progress_circle _colored"></p>
+                        <p>약관 동의</p>
+                    </div>
+                    <div>
+                        <p className="progress_circle"></p>
+                        <p>정보 입력</p>
+                    </div>
+                    <div>
+                        <p className="progress_circle"></p>
+                        <p>신청 완료</p>
+                    </div>
                 </div>
             </section>
-            <section>
-                <h3><input type="checkbox" checked={chk1} onChange={()=>{ setChk1(!chk1)}}/>[필수] 서비스이용약관</h3>
-                <div>
+            <section className="agree_tit">
+                <h3>약관 동의</h3>
+                <p>약관 및 개인정보 수집 이용에 동의 해주세요.</p>
+                <div className="allagree_check checkbox_wrap">
+                    <input id="agree" type="checkbox" checked={allChk} onChange={handleAllChkChange}/>
+                    <label for="agree"><span>이용약관 동의(필수) 및 개인정보 수집 이용 동의(필수)에 모두 동의합니다.</span></label>
+                </div>
+            </section>
+            <section className="agree_box">
+                <div className="agree_check checkbox_wrap">
+                    <input id="chk1" type="checkbox" checked={chk1} onChange={()=>{ setChk1(!chk1)}}/>
+                    <label for="chk1"><span>[필수]</span> 서비스이용약관</label>
+                </div>
+                <div className="policy_box">
                     <h4>제 1장 총칙</h4>
                     <h5>제 1 조 목적</h5>
                     <p>이 이용약관은 (주)대한항공(이하 "대한항공"이라 합니다)이 운영하는 대한항공 온라인 플랫폼에서에서 제공하는 온라인 서비스(이하 "서비스"라 합니다)를
@@ -55,8 +74,12 @@ function AgreementStep({onNext,setStep}){
                     </p>
                 </div>
             </section>
-            <section>
-                <h3><input type="checkbox" checked={chk2} onChange={()=>{setChk2(!chk2)}}/>[필수] 개인정보처리방침</h3>
+            <section className="agree_box">
+                <div className="agree_check checkbox_wrap">
+                    <input id="chk2" type="checkbox" checked={chk2} onChange={()=>{setChk2(!chk2)}}/>
+                    <label for="chk2"><span>[필수]</span> 개인정보처리방침</label>
+                </div>
+                <div className="policy_box">
                     <h4>제 1장 총칙</h4>
                     <h5>제1조 개인정보처리방침의 의의</h5>
                     <p>본 개인정보처리방침은 (주)대한항공(이하"대한항공")의 개인정보 주체와 관련된 개인정보 처리 및 보호와 정보통신망 안전성확보에 대하여 규정하고 있습니다.
@@ -76,9 +99,9 @@ function AgreementStep({onNext,setStep}){
                     3. 당 사이트가 전항에 따라 개정약관을 공지하면서 “개정일자 적용 이전까지 회원이 명시적으로 거부의 의사표시를 하지 않는 경우 회원이 개정약관에 동의한 것으로 봅니다. ”라는 취지를 명확하게 공지하였음에도 회원이 명시적으로 거부의 의사표시를 하지 않은 경우에는 개정약관에 동의한 것으로 봅니다. 회원이 개정약관에 동의하지 않는 경우 당 사이트 이용계약을 해지할 수 있습니다.
                     </p>
                     <p>서비스를 이용하기 위해서는 동의가 필요합니다.</p>
-
+                </div>
             </section>
-            <button  onClick={handleNextBtnChange}>다음</button>
+            <div className="agree_btn"><button  onClick={handleNextBtnChange} className="btn_blue">다음</button></div>
         </div>
     )
 }
--- src/page/detail/CompletionStep.js
+++ src/page/detail/CompletionStep.js
@@ -1,7 +1,41 @@
+import './CompletionStep.css';
+
 function CompletionStep(){
     return(
         <div>
-            <h1>CompletionStep</h1>
+            <section className="agree_progress">
+                <div className="progress_wrap">
+                    <div>
+                        <p className="progress_circle"></p>
+                        <p>약관 동의</p>
+                    </div>
+                    <div>
+                        <p className="progress_circle"></p>
+                        <p>정보 입력</p>
+                    </div>
+                    <div>
+                        <p className="progress_circle _colored"></p>
+                        <p>신청 완료</p>
+                    </div>
+                </div>
+            </section>
+            <section className='completecon_wrap'>
+                <div className='complete_con01'>
+                    <ul>
+                        <li className='complete_tit'>방문신청이 완료되었습니다.</li>
+                        <li><span>승인이 완료되면</span> 방문 신청이 최종 완료됩니다.<br />
+                        신청내역과 승인 상황은 <span className='go_search' onClick={(e)=>{window.location.href='/search'}}>방문신청 조회 페이지</span>에서 확인하실 수 있습니다.</li>
+                    </ul>
+                </div>
+                <div className='complete_con02'>
+                    <div className='visit_caution'>방문 시 필요 사항</div>
+                    <ul>
+                        <li>방문 시 먼저 <span>초소 안내센터</span>로 방문해주세요.</li>
+                        <li>매일 06시 QR코드가 폐기됩니다.  (* 1개월간 폐기 유예)</li>
+                    </ul>
+                </div>
+            </section>
+            <div className="agree_btn"><button  onClick={(e)=>{window.location.href = '/'}} className="btn_blue">확인</button></div>
         </div>
     )
 }
