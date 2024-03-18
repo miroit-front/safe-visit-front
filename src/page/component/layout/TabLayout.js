aee914eb2e6b50db4f3c266ea34ed9e8788a3d0b
yoohwajoo <hwahjoo@gmail.com>
2024-03-18 17:19:44 +0900

    탭메뉴를 페이지 왔을때 활성화되게 코드 수정

--- src/page/component/layout/TabLayout.js
+++ src/page/component/layout/TabLayout.js
@@ -1,14 +1,15 @@
+import { NavLink } from 'react-router-dom';
 import '../page/FooterContent.css';
- 
- function TabLayout({tab}){
-    return(
+
+function TabLayout({ tab }) {
+    return (
         <nav>
-        <ul className="ftcon_tab">
-            <li className={tab===0 ? 'active' : ''} onClick={()=>{window.location.href = '/termOfService'}}>서비스이용약관</li>
-            <li className={tab===1 ? 'active' : ''}  onClick={()=>{window.location.href = '/privacyPolicy'}}>개인정보처리방침</li>
-            <li className={tab===2 ? 'active' : ''}  onClick={()=>{window.location.href = '/faq'}}>FAQ</li>
-         </ul>
-     </nav>
+            <div className="ftcon_tab">
+                <NavLink to="/termOfService" activeClassName="active"><button>서비스이용약관</button></NavLink>
+                <NavLink to="/privacyPolicy" activeClassName="active"><button>개인정보처리방침</button></NavLink>
+                <NavLink to="/faq" activeClassName="active"><button>FAQ</button></NavLink>
+            </div>
+        </nav>
     )
 }
 
