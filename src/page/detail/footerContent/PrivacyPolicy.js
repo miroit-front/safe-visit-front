7d3329e63eef49003e432004e19e5168e355d738
yoohwajoo <hwahjoo@gmail.com>
2024-03-19 10:57:45 +0900

    개인정보처리방침 셀렉트박스/각 옵션별 컴포넌트 생성

--- src/page/detail/footerContent/PrivacyPolicy.js
+++ src/page/detail/footerContent/PrivacyPolicy.js
@@ -1,6 +1,37 @@
+import React, { useState, useRef, useEffect } from 'react';
 import TabLayout from "../../component/layout/TabLayout";
+import Policycon01 from "./Policycon01";
+import Policycon02 from "./Policycon02";
 
 function PrivacyPolicy(){
+
+    const [menu, setMenu] = useState(['개인정보처리방침 2023.10.31', '개인정보처리방침 0000.00.00']);
+    const [selectedOption, setSelectedOption] = useState('개인정보처리방침 2023.10.31');
+    const [showMenu, setShowMenu] = useState(false);
+    const selectBoxRef = useRef(null);
+
+    const handleSelect = (option) => {
+        setSelectedOption(option);
+        setShowMenu(false); // 옵션 선택 후 메뉴를 닫음
+    }
+
+    const handleSelectBoxToggle = () => {
+        setShowMenu(!showMenu); // 옵션 선택 상자를 열거나 닫음
+    }
+
+    useEffect(() => {
+        function handleClickOutside(event) {
+            if (selectBoxRef.current && !selectBoxRef.current.contains(event.target)) {
+                setShowMenu(false);
+            }
+        }
+
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [selectBoxRef]);
+
     return(
         <div className="inner">
             <TabLayout/>
@@ -9,23 +40,25 @@ function PrivacyPolicy(){
                     <h3>개인정보처리방침</h3>
                     <p>대한항공 방문 예약 시스템은 개인정보보호법률을 준수하고 있습니다.</p>
                 </div>
-                <div className="selectBox">
-                    <select className="selectBtn">선택
-                        <option value="title">개인정보처리방침 2023.10.31</option>
-                        <option className="content">개인정보처리방침 2023.10.31</option>
-                    </select>
+                <div className='selectwrap' ref={selectBoxRef}>
+                    <div className={`select-box ${showMenu ? 'open' : ''}`} onClick={handleSelectBoxToggle}>
+                        {selectedOption}
+                    </div>
+                    {showMenu && (
+                        <div className="select-options">
+                            {menu.map((item, index) => (
+                                <div key={index} className={`option-item ${item === selectedOption ? 'select' : ''}`}
+                                    onClick={() => handleSelect(item)}>
+                                    {item}
+                                </div>
+                            ))}
+                        </div>
+                    )}
                 </div>
             </div>
             <div className="footercon_wrap">
-                <h4>제 1 장 총칙</h4>
-                <h5>제1조 개인정보처리방침의 의의</h5>
-                <p>본 개인정보처리방침은 ㈜대한항공(이하”대한항공”)의 개인정보 주체와 관련된 개인정보 처리 및 보호와 정보통신망 안전성확보에 대하여 규정하고 있습니다.</p>
-                <p>개인정보 주체는 개인정보에 접근하거나, 수정 또는 삭제, 개인정보의 처리를 제한할 수 있는 권리를 포함한 여러 가지 권리를 지닙니다. 정보주체에 대해 처리하는 개인정보, 처리 목적 및 법적 근거, 정보주체의 권리를 본 방침의 각 조항을 클릭하여 확인하실 수 있습니다.</p>
-                <p>정보주체의 개인정보와 당사의 개인정보 보호 능력에 대한 정보주체의 신뢰는 대한항공 사업의 핵심입니다. 대한항공은 정보주체의 개인정보를 보호하고, 불법적인 개인정보유출 및 손해 발생을 방지함에 최선의 노력을 다하겠습니다.</p>
-                <p>대한항공의 개인정보처리방침은 개인정보보호법 등 국내의 개인정보 보호 법령에 근거하며, 법률의 제·개정 및 정부지침의 변경 또는 대한항공 내부 방침의 변경에 따라 바뀔 수 있으므로 수시로 확인하여 주시기 바랍니다.</p>
-                <h5>제2조 (용어의 정의)</h5>
-                <p>1. 개인정보의 정의<br />
-                    "개인정보"란 식별된 또는 식별 가능한 개인과 직간접적으로 연관된 모든 정보를 의미합니다.</p>
+                {selectedOption === '개인정보처리방침 2023.10.31' && <Policycon02 />}
+                {selectedOption === '개인정보처리방침 0000.00.00' && <Policycon01 />}
             </div>
         </div>
     )
